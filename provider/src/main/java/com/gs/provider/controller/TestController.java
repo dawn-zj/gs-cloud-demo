c96c8c177f14c30a560bb32d5a96a23334f75f31
dawn-zj <zj19970818>
2020-11-01 15:36:50 +0800

    创建子工程

---
+++ provider/src/main/java/com/gs/provider/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.gs.provider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "Hello World";
+    }
+}
