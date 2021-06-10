54041bb0282fec641afbcc207ca4bf24030af03d
dawn-zj <867096367@qq.com>
2021-06-10 14:44:16 +0800

    读取配置中的port，测试负载均衡

--- provider/src/main/java/com/gs/provider/controller/TestController.java
+++ provider/src/main/java/com/gs/provider/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.gs.provider.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${server.port:8080}")
+    private int port;
 
     @GetMapping("/index")
     public String index() {
-        return "Hello World";
+        return "Hello World! Port: " + port;
     }
 }
