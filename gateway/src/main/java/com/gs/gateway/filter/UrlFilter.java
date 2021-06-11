00870ac6b47aaf4fca42ebec79e8b679ebf5badf
dawn-zj <867096367@qq.com>
2021-06-11 14:52:59 +0800

    编写过滤器拦截

---
+++ gateway/src/main/java/com/gs/gateway/filter/UrlFilter.java
@@ -0,0 +1,21 @@
+package com.gs.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class UrlFilter implements GlobalFilter {
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		ServerHttpRequest request = exchange.getRequest();
+		String url = request.getURI().getPath();
+		//TODO 拦截特定URL地址
+		System.out.println("url: " + url);
+		return chain.filter(exchange);
+	}
+}
